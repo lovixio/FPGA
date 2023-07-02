e06d17f9419c5e06472f28eac0d7981bdd6aaf92
Leon Herrera <leonherrera@amalgama.co>
2023-07-02 15:14:03 -0300

    button_reader: resuelve el read_enable cada clock en vez de cada decasegundo

--- TP/button_reader.vhd
+++ TP/button_reader.vhd
@@ -44,63 +44,86 @@ architecture button_reader_architecture of button_reader is
         );
         
         --proceso para ver que tipo de señal es y contarlo.
+
+        --proceso que maneja los cambios de los registros luego de mandar a leer una codigo.
+        -- si estamos escribiendo y acabamos de mandar a leer una señal:
+        -- si la señal nueva es un silencio, reseteamos en 0 todo
+        -- si las señal nueva es un short/long, ponemos lo especulado en current y reseteamos possibles.
+        process(clock_in)
+        begin
+            if(enable_in = '1' and read_enable = '1') then
+                if(possible_type = '0') then
+                    current_duration <= (others => '0');
+                    current_type <= '0';
+                else
+                    current_duration <= possible_duration;
+                    current_type <= '1';
+                    possible_type <= '0';
+                    possible_duration <= (others => '0');
+                end if; 
+                read_enalbe <= '0'; 
+            end if;
+        end process;
+
+        -- proceso para contar cada decasegundo de señales.
+        -- si hay que resetear, reseteamos todo.
+        -- si pasó un decasegundo y estamos escribiendo:
+        --      si estamos especulando un posible short/long, añadimos al contador posible.
+        --      si ese contador llegó al short, dejamos que se lea el silencio anterior y decrementamos el current con el short.
+        --      movemos el contador current.
         process(one_decasecond_passed, reset_in)
         begin
             if(reset_in = '1') then
                 current_duration <= (others => '0');                    
                 current_type <= '0';
+                read_enable <= '0';
+                possible_type <= '0';
+                possible_duration <= (others => '0');
             end if;
+
             if(one_decasecond_passed = '1' and enable_in = '1') then
 
-                if(read_enable = '1' and possible_type = '0') then
-                    next_duration <= (others => '0') + 1;
-                    current_type <= '0';
-                    read_enalbe <= '0';
-                end if;
-
                 if(possible_type = '1') then
-                    if(possible_duration < short_limit_in) then
-                        possible_duration <= possible_duration +1;
-                    elsif(read_enable = '0') then
-                        possible_duration <= possible_duration +1;
+
+                    possible_duration <= possible_duration +1;
+
+                    if(possible_duration > short_limit_in-1) then
                         read_enable <= '1';
-                    else
-                        next_duration <= possible_duration;
-                        current_type <= '1';
-                        possible_type <= '0';
-                        read_enable <= '0';            
+                        next_duration <= next_duration - possible_duration;          
                     end if;
+
                 end if;       
+
                 current_duration <= next_duration;
+
             end if;
         end process;
         
-        --TODO: solo confirmar el cambio del type cuando tenemos 1 short-limit de señal. 
-        -- Necesitamos un contador paralelo para esto. No paramos el contador principal
-        -- si llegamos al sortlimit, hacemos el cambio, restamos del principal un short-limit y enviamos el silencio.
-        -- sino, reseteamos el contador paralelo y seguimos contando silencio.
+        --proceso para controlar la señal que nos llega del botón:
+        --  si estamos escribiendo:
+        --      si la señal nueva es un short/long:
+        --          lo ponemos como posible y empezamos a contarlo.
+        --      si no: ponemos posible en 0 y mandamos read_enable para leer el short/long que veniamos viendo si había.
         process(button_in)
         begin
             if(enable_in = '1') then
                 if(button_in = '1') then
                     possible_type <= '1';
-                    possible_duration <= (others => '0');
                 else
-                    if(read_enable = '1') then
-                        current_duration <= possible_duration;
-                        current_type <= possible_type
-                    else
+                    possible_duration <= (others => '0');
+                    possible_type <= '0';
+                    if(current_type = '1') then
                         read_enable <= '1';
                     end if;
-                    possible_type <= '0';
                 end if;
             end if;
         end process;
                         
-                                
+    -- si current está en su maximo, dejamos next estatico. si no, le sumamos 1.                            
     next_duration <= current_duration when current_duration = unsigned(max_count) else 
     current_duration + 1;
     
+    --ponemos el out los current y el read_enable.
     duration_out <= current_duration;
     type_out <= current_type;
     read_enable_out <= read_enable;
